47ec03a945035247274493cdc7c2468601e85612
wildskyf <poppin.wildsky+gh@gmail.com>
2019-03-04 16:36:18 +0800

    [Bugfix] cleanup useless iframe in camera callback function

--- src/containers/FirstPopupModal.js
+++ src/containers/FirstPopupModal.js
@@ -45,15 +45,15 @@ class FirstPopupModal extends PureComponent {
 
   openCamera() {
     let that = this;
+    let iframe = document.createElement('IFRAME');
+    iframe.setAttribute('src', 'opencamera://');
+
     window.getQRCode = code => {
       that.setState({nodeId: code});
+      iframe.remove();
     };
 
-    let url = 'opencamera://';
-    var iframe = document.createElement("IFRAME");
-    iframe.setAttribute("src", url);
     document.documentElement.appendChild(iframe);
-    iframe = null;
   }
 
   onNameChange(e) {
