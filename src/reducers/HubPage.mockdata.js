5cfb240fada622478dc3046350580dfd426d1819
wildskyf <poppin.wildsky+gh@gmail.com>
2019-03-20 17:08:40 +0800

    [Test] add testcases for reducer/HubPage

--- src/reducers/HubPage.mockdata.js
+++ src/reducers/HubPage.mockdata.js
@@ -1,40 +1,74 @@
 import * as utils       from './utils'
 import * as constants   from '../constants/Constants'
 
-export const testBoard = {
+const commonInfo = {
+  Title:            'dGVzdCBib2FyZA==', // test board
+  C:                '8PekZyubu7TRhBZkB4BcWoYHBH1GAb5Cy1qXesUXwTdnphhuzfhXbRS',
+  CreateTS:         utils.emptyTimeStamp(),
+  UpdateTS:         utils.emptyTimeStamp(),
+  JT:               utils.emptyTimeStamp(),
+  ArticleCreateTS:  utils.emptyTimeStamp(),
+  LastSeen:         utils.emptyTimeStamp(),
+}
 
-    content_getBoardList: [
-        {
-          ID:               '948UChtsWe1EdMpYdfikv7eGbFJE9nmXvdiTXJ3VvEfoiSAQqqNtd8Q',
-          Title:            'SGVsbG8gTXkgQm9hcmQ=', // Hello My Board
-          S:                constants.STATUS_ARRAY.indexOf('StatusAlive'),
-          CreateTS:         utils.emptyTimeStamp(),
-          UpdateTS:         utils.emptyTimeStamp(),
-          JT:               utils.emptyTimeStamp(),
-          ArticleCreateTS:  utils.emptyTimeStamp(),
-          LastSeen:         utils.emptyTimeStamp(),
-          C:               '8PekZyubu7TRhBZkB4BcWoYHBH1GAb5Cy1qXesUXwTdnphhuzfhXbRS',
-          BT:               constants.ENTITY_TYPE_ARRAY.indexOf('EntityTypePersonal'),
-        }
-    ],
+const personalBoard = {
+  ID:               '948UChtsWe1EdMpYdfikv7eGbFJE9nmXvdiTXJ3VvEfoiSAQqqNtd8Q',
+  S:                constants.STATUS_ARRAY.indexOf('StatusAlive'),
+  BT:               constants.ENTITY_TYPE_ARRAY.indexOf('EntityTypePersonal'),
+  ...commonInfo
+}
 
-    me_getBoardRequests: [
-    ],
+const privateBoard = {
+  ID:               '7b1WuAJhRh3ECAUyGv8ekXPLywDPbEDR41CAjjwHJoujiiRfz8UTz7E',
+  S:                constants.STATUS_ARRAY.indexOf('StatusAlive'),
+  BT:               constants.ENTITY_TYPE_ARRAY.indexOf('EntityTypePrivate'),
+  ...commonInfo
+}
 
-    account_getUserNameByIDs: [
-        {
-            ID:   '8PekZyubu7TRhBZkB4BcWoYHBH1GAb5Cy1qXesUXwTdnphhuzfhXbRS',
-            N:    'c2FtbXVp', //sammui
-        }
-    ],
+const deletedPrivateBoard = {
+  ID:               '7b1WuAJhRh3ECAUyGv8ekXPLywDPbEDR41CAjjwHJoujiiRfz8UTz7E',
+  S:                constants.STATUS_ARRAY.indexOf('StatusDeleted'),
+  BT:               constants.ENTITY_TYPE_ARRAY.indexOf('EntityTypePrivate'),
+  ...commonInfo
+}
 
-    account_getUserImgByIDs: [
-        {
-            ID:     '8PekZyubu7TRhBZkB4BcWoYHBH1GAb5Cy1qXesUXwTdnphhuzfhXbRS',
-            T:      constants.IMG_TYPE_ARRAY.indexOf('ImgTypeJPEG'),
-            I:      '', // no image
-            W:      100,
-            H:      100,
-        }
-    ],
+const userName = {
+  ID:   '8PekZyubu7TRhBZkB4BcWoYHBH1GAb5Cy1qXesUXwTdnphhuzfhXbRS',
+  N:    'c2FtbXVp', // sammui
+}
+
+const userImg = {
+    ID:     '8PekZyubu7TRhBZkB4BcWoYHBH1GAb5Cy1qXesUXwTdnphhuzfhXbRS',
+    T:      constants.IMG_TYPE_ARRAY.indexOf('ImgTypeJPEG'),
+    I:      '', // no image
+    W:      100,
+    H:      100,
+}
+
+export const testNoBoard = {
+    content_getBoardList: [ ],
+    me_getBoardRequests: [ ],
+    account_getUserNameByIDs: [ userName ],
+    account_getUserImgByIDs: [ userImg ],
+}
+
+export const testPrivateBoard = {
+    content_getBoardList: [ privateBoard ],
+    me_getBoardRequests: [ ],
+    account_getUserNameByIDs: [ userName ],
+    account_getUserImgByIDs: [ userImg ],
+}
+
+export const testPersonalBoard = {
+    content_getBoardList: [ personalBoard ],
+    me_getBoardRequests: [ ],
+    account_getUserNameByIDs: [ userName ],
+    account_getUserImgByIDs: [ userImg ],
+}
+
+export const testDeletedPrivateBoard = {
+    content_getBoardList: [ deletedPrivateBoard ],
+    me_getBoardRequests: [ ],
+    account_getUserNameByIDs: [ userName ],
+    account_getUserImgByIDs: [ userImg ],
 }
--- src/reducers/HubPage.test.js
+++ src/reducers/HubPage.test.js
@@ -14,18 +14,16 @@ import * as constants   from '../constants/Constants'
 
 const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
 
-let store       = null
 let rootUUID    = getUUID()
 let pageUUID    = getUUID()
 
-describe('test Board', () => {
+describe('Using getBoardList()', () => {
 
-    beforeAll(async () => {
-
+    const setup = async mockData => {
         let mockQuery   = {}
         let paramQuery  = {}
 
-        store = setupStore(HubpageMockData.testBoard);
+        const store = setupStore(mockData);
 
         // init root page
         await store.dispatch(utils.init({rootUUID, myClass, myDuck, ...mockQuery, ...paramQuery}))
@@ -33,13 +31,11 @@ describe('test Board', () => {
 
         // init hubpage
         await store.dispatch(init(pageUUID, rootUUID, myClass, myDuck))
-    });
+        return store
+    }
 
-    afterAll(() => {
-        store = null
-    })
-
-    test.skip('test getBoardList()', async () => {
+    it('should find no board when fetching nothing', async () => {
+        const store = await setup(HubpageMockData.testNoBoard)
 
         await store.dispatch(getBoardList(pageUUID, true, constants.NUM_BOARD_PER_REQ))
 
@@ -47,12 +43,17 @@ describe('test Board', () => {
 
         let hubPage     = store.getState()['hubPage'].get(pageUUID, Immutable.Map())
         let boardList   = hubPage.get('boardList', Immutable.List()).toJS()
+        let noBoard     = hubPage.get('noBoard')
 
-        expect(boardList).toHaveLength(1);
-
+        expect(boardList).toHaveLength(0);
+        expect(noBoard).toBe(true);
     })
 
-    test.skip('test getBoardList() 2', async () => {
+    // BoardType could be personal, private, and public
+    // getBoardList will only select boards whose type is private
+
+    it('should find one board when fetching one private board', async () => {
+        const store = await setup(HubpageMockData.testPrivateBoard)
 
         await store.dispatch(getBoardList(pageUUID, true, constants.NUM_BOARD_PER_REQ))
 
@@ -60,11 +61,57 @@ describe('test Board', () => {
 
         let hubPage     = store.getState()['hubPage'].get(pageUUID, Immutable.Map())
         let boardList   = hubPage.get('boardList', Immutable.List()).toJS()
+        let noBoard     = hubPage.get('noBoard')
 
         expect(boardList).toHaveLength(1);
+        expect(noBoard).toBe(false);
+    })
 
+    it('should find no board when fetching personal board', async () => {
+        const store = await setup(HubpageMockData.testPersonalBoard)
+
+        await store.dispatch(getBoardList(pageUUID, true, constants.NUM_BOARD_PER_REQ))
+
+        await delay(20); /* wait long enough for state to be updated */
+
+        let hubPage     = store.getState()['hubPage'].get(pageUUID, Immutable.Map())
+        let boardList   = hubPage.get('boardList', Immutable.List()).toJS()
+        let noBoard     = hubPage.get('noBoard')
+
+        expect(boardList).toHaveLength(0);
+        expect(noBoard).toBe(true);
+    })
+
+    // deleted board should not be counted, even if it is private
+
+    it('should find no board when fetching deleted private board', async () => {
+        const store = await setup(HubpageMockData.testDeletedPrivateBoard)
+
+        await store.dispatch(getBoardList(pageUUID, true, constants.NUM_BOARD_PER_REQ))
+
+        await delay(20); /* wait long enough for state to be updated */
+
+        let hubPage     = store.getState()['hubPage'].get(pageUUID, Immutable.Map())
+        let boardList   = hubPage.get('boardList', Immutable.List()).toJS()
+        let noBoard     = hubPage.get('noBoard')
+
+        expect(boardList).toHaveLength(0);
+        expect(noBoard).toBe(true);
+    })
+
+    it('should find no board when fetch nothing not for the first time', async () => {
+        const store = await setup(HubpageMockData.testNoBoard)
+
+        await store.dispatch(getBoardList(pageUUID, false, constants.NUM_BOARD_PER_REQ))
+
+        await delay(20); /* wait long enough for state to be updated */
+
+        let hubPage     = store.getState()['hubPage'].get(pageUUID, Immutable.Map())
+        let boardList   = hubPage.get('boardList', Immutable.List()).toJS()
+        let noBoard     = hubPage.get('noBoard')
+
+        expect(boardList).toHaveLength(0);
+        expect(noBoard).toBe(true);
     })
 
 })
-
-
