c0fdec6cc2db95ab287cc59c5fe66dd220f66751
wildskyf <poppin.wildsky+gh@gmail.com>
2019-04-19 14:19:41 +0800

    [Refactor] rename markSeen function to checkMarkFriendlistSeen

--- src/containers/RootPage.js
+++ src/containers/RootPage.js
@@ -177,7 +177,7 @@ class RootPage extends PureComponent {
     }
   }
 
-  markSeen() {
+  checkMarkFriendListSeen() {
     let { myId, actions: {doRootPage}, match:{params} } = this.props
 
     let me                  = getRoot(this.props)
@@ -312,7 +312,7 @@ class RootPage extends PureComponent {
             MAIN_PAGE = (<FriendListPage {...this.props} markSeen={markFriendRead} myId={friendListPageId}/>)
             break;
         case 'FriendChatPage':
-            MAIN_PAGE = (<FriendChatPage {...this.props} markSeen={this.markSeen} myId={friendChatPageId}/>)
+            MAIN_PAGE = (<FriendChatPage {...this.props} markSeen={this.checkMarkFriendListSeen} myId={friendChatPageId}/>)
             break;
         default:
             MAIN_PAGE = null
