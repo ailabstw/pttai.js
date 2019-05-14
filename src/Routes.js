05638ecba629dfd69177bcd6c0713eb3a4aa1645
wildskyf <poppin.wildsky+gh@gmail.com>
2019-05-14 16:51:05 +0800

    [Refactor] minor adjust

--- src/Routes.js
+++ src/Routes.js
@@ -1,24 +1,18 @@
-import React, { Component } from 'react'
-import { PTTAI_URL_BASE }   from 'config'
+import React from 'react'
+import { PTTAI_URL_BASE } from 'config'
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
 
 import RootPage from './containers/RootPage'
 
-class Routes extends Component {
-  render() {
-    return (
-      <Router basename={`${PTTAI_URL_BASE}`}>
-        <Switch>
-          <Route exact path={`/hub`} render={(props) => <RootPage {...props} myComponent={'HubPage'} />} />
-          <Route exact path={`/board/:boardId`} render={(props) => <RootPage {...props} myComponent={'BoardPage'} />} />
-          <Route exact path={`/board/:boardId/article/:articleId`} render={(props) => <RootPage {...props} myComponent={'ArticlePage'} />} />
-          <Route exact path={`/friend`} render={(props) => <RootPage {...props} myComponent={'FriendListPage'} />} />
-          <Route exact path={`/friend/:friendId/chat/:chatId`} render={(props) => <RootPage {...props} myComponent={'FriendChatPage'} />} />
-          <Redirect exact from={`/`} to={`/friend`} />
-        </Switch>
-      </Router>
-    );
-  }
-}
-
-export default Routes
+export default () => (
+  <Router basename={`${PTTAI_URL_BASE}`}>
+    <Switch>
+      <Route exact path={`/hub`} render={(props) => <RootPage {...props} myComponent={'HubPage'} />} />
+      <Route exact path={`/board/:boardId`} render={(props) => <RootPage {...props} myComponent={'BoardPage'} />} />
+      <Route exact path={`/board/:boardId/article/:articleId`} render={(props) => <RootPage {...props} myComponent={'ArticlePage'} />} />
+      <Route exact path={`/friend`} render={(props) => <RootPage {...props} myComponent={'FriendListPage'} />} />
+      <Route exact path={`/friend/:friendId/chat/:chatId`} render={(props) => <RootPage {...props} myComponent={'FriendChatPage'} />} />
+      <Redirect exact from={`/`} to={`/friend`} />
+    </Switch>
+  </Router>
+)
