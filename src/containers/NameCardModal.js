68ada2bcdfcb6c6a89d0f817cd0bec8b620240bb
wildskyf <poppin.wildsky+gh@gmail.com>
2019-05-20 17:27:12 +0800

    [Bugfix] find icons back
    ref: https://facebook.github.io/create-react-app/docs/adding-images-fonts-and-files

--- src/containers/NameCardModal.js
+++ src/containers/NameCardModal.js
@@ -8,8 +8,6 @@ import { FormattedMessage,
 import validator from 'validator'
 import Immutable from 'immutable'
 
-import { PTTAI_URL_BASE } from '../config'
-
 import AlertComponent from '../components/AlertComponent'
 
 import * as modalConstants from '../constants/ModalConstants'
@@ -20,6 +18,7 @@ import { getOrientation,
 import googleAnalytics from '../utils/googleAnalytics'
 
 import styles from './NameCardModal.module.css'
+import qr_code_icon from './../images/btn_qrcode@2x.jpg'
 
 function isEmpty (name) {
   return name.replace(/\s\s+/g, '') === ''
@@ -122,7 +121,7 @@ class NameCardModal extends PureComponent {
                 {
                   isEditable && (
                     <div className={styles['qr-code']} onClick={this.openQRCodeModal}>
-                      <img src={`${PTTAI_URL_BASE}/images/btn_qrcode@2x.jpg`} alt='QRCode Button' />
+                      <img src={qr_code_icon} alt='QRCode Button' />
                     </div>)
                 }
 
@@ -495,7 +494,7 @@ class EditingNameCard extends PureComponent {
               <input type='file' id='getval' onChange={this.onUpload} />
             </label>
             <div className={styles['qr-code']}>
-              <img src={`${PTTAI_URL_BASE}/images/btn_qrcode@2x.jpg`} alt='QRCode Button' />
+              <img src={qr_code_icon} alt='QRCode Button' />
             </div>
           </div>
 
