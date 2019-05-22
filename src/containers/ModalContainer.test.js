01b935cc005a527458058582c7cf08a4b7a5597b
wildskyf <poppin.wildsky+gh@gmail.com>
2019-05-22 16:01:38 +0800

    [Chore] revert '[Hotfix] ignore some test'
    Ref: b230072566cec1c03c5f4efefc6ada6107c555be

--- src/containers/ModalContainer.test.js
+++ src/containers/ModalContainer.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDom from 'react-dom'
-// import ModalContainer from './ModalContainer'
+import ModalContainer from './ModalContainer'
 
 it.skip('should pass', () => {
 })
