4f573a1685e9a2d7f303ab541c06a5b5df003198
wildskyf <poppin.wildsky+gh@gmail.com>
2019-05-24 14:49:35 +0800

    [Test] add some test for container/HubPage

--- src/containers/HubPage.test.js
+++ src/containers/HubPage.test.js
@@ -1,47 +1,53 @@
 import React from 'react'
-import ReactDom from 'react-dom'
+import { IntlProvider } from 'react-intl'
 import { Provider } from 'react-redux'
 import { mount, unmount } from 'enzyme'
 
 import HubPage from './HubPage'
+import Empty from '../components/Empty'
 import HubComponent from '../components/HubComponent'
-import BoardListComponent from '../components/BoardListComponent'
+import AlertComponent from '../components/AlertComponent'
 
-import { getUUID } from '../utils/utils'
+import { language, messages, getUUID } from '../utils/utils'
 import setupStore from './testUtils'
 
 let store = null
 
-const hubPageId = getUUID()
-
 describe('HubPage container', () => {
-  // let wrapper = null
+  let wrapper = null
+  let domTreeFunc = (id, props) => (
+    <IntlProvider locale={language} messages={messages}>
+      <Provider store={store}>
+        <HubPage {...props} markSeen={()=>{}} myId={id} />
+      </Provider>
+    </IntlProvider>
+  )
 
-  // beforeAll(() => {
-  //     store = setupStore();
-  // });
-
-  // afterAll(() => {
-  //     store = null
-  // });
-
-  // beforeEach(() => {
-  //     wrapper = mount(
-  //       <Provider store={store}>
-  //         <HubPage myId={hubPageId}/>
-  //       </Provider>
-  //     );
-  // })
-
-  // afterEach(() => {
-  //     wrapper.unmount()
-  // })
-
-  it('should render the hubpage container', async () => {
-
-    // expect(wrapper.find(HubPage).length).toBe(1);
-    // expect(wrapper.find(HubComponent).length).toBe(1);
-    // expect(wrapper.find(BoardListComponent).length).toBe(1);
-
+  beforeAll(() => {
+    store = setupStore();
   })
+
+  afterAll(() => {
+    store = null
+  })
+
+  afterEach(() => {
+    wrapper.unmount()
+  })
+
+  it('should render nothing if no hubpage id', () => {
+    wrapper = mount(domTreeFunc(null));
+
+    expect(wrapper.find(Empty).length).toBe(1);
+    expect(wrapper.find(HubComponent).length).toBe(0);
+  })
+
+  it('should render the hubpage container and its components', () => {
+    wrapper = mount(domTreeFunc(getUUID()));
+
+    expect(wrapper.find(HubComponent).length).toBe(1);
+    expect(wrapper.find(AlertComponent).length).toBe(1);
+  })
+
+
 })
