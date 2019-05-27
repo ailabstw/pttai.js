aff7da2b3d9072307e78b11efa4cf071ac297622
wildskyf <poppin.wildsky+gh@gmail.com>
2019-05-27 16:18:18 +0800

    [Test] add test for utils/notification

--- src/utils/notification.js
+++ src/utils/notification.js
@@ -1,14 +1,14 @@
 export const setup = () => {
   if (!('Notification' in window)) {
     console.log('This browser does not support notification')
-    return Promise.reject()
+    return Promise.reject('Browser Not Support')
   }
 
   if (Notification.permission === 'denied') {
-    return Promise.reject()
+    return Promise.reject('Permission Denied')
   }
 
-  if (Notification.permission === 'default' || Notification.permission === 'undefined') {
+  if (Notification.permission === 'default' || Notification.permission === undefined) {
     return Notification.requestPermission()
   }
 
@@ -16,14 +16,18 @@ export const setup = () => {
   return Promise.resolve()
 }
 
-export const show = ({ title, body, icon }) => {
+export const show = (params) => {
+  const { title, body, icon } = params || {}
+
   if (!title) {
-    return console.error(`TypeError: Wrong format. Param should be an object: {
-        title: <String>,
-        body: <String>,
-        icon: <URL>,
-        tag: <noti_id>
+    console.error(`TypeError: Wrong format. Param should be an object: {
+      title: <String>,
+      body: <String>,
+      icon: <URL>,
+      tag: <noti_id>
     }`)
+
+    return false
   }
 
   const createNoti = () => new Notification(title, { body, icon })
