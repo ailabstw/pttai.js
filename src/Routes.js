b4d46819b1bfc3abc65b3e63d1f5efcb966be9f9
wildskyf <poppin.wildsky+gh@gmail.com>
2019-06-04 16:36:32 +0800

    [Test] add test case for Routes

--- src/Routes.js
+++ src/Routes.js
@@ -4,15 +4,20 @@ import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-d
 
 import RootPage from './containers/RootPage'
 
+// for testing
+export const SwitchRoutes = () => (
+  <Switch>
+    <Route exact path={`/hub`} render={(props) => <RootPage {...props} myComponent={'HubPage'} />} />
+    <Route exact path={`/board/:boardId`} render={(props) => <RootPage {...props} myComponent={'BoardPage'} />} />
+    <Route exact path={`/board/:boardId/article/:articleId`} render={(props) => <RootPage {...props} myComponent={'ArticlePage'} />} />
+    <Route exact path={`/friend`} render={(props) => <RootPage {...props} myComponent={'FriendListPage'} />} />
+    <Route exact path={`/friend/:friendId/chat/:chatId`} render={(props) => <RootPage {...props} myComponent={'FriendChatPage'} />} />
+    <Redirect exact from={`/`} to={`/friend`} />
+  </Switch>
+)
+
 export default () => (
   <Router basename={`${PTTAI_URL_BASE}`}>
-    <Switch>
-      <Route exact path={`/hub`} render={(props) => <RootPage {...props} myComponent={'HubPage'} />} />
-      <Route exact path={`/board/:boardId`} render={(props) => <RootPage {...props} myComponent={'BoardPage'} />} />
-      <Route exact path={`/board/:boardId/article/:articleId`} render={(props) => <RootPage {...props} myComponent={'ArticlePage'} />} />
-      <Route exact path={`/friend`} render={(props) => <RootPage {...props} myComponent={'FriendListPage'} />} />
-      <Route exact path={`/friend/:friendId/chat/:chatId`} render={(props) => <RootPage {...props} myComponent={'FriendChatPage'} />} />
-      <Redirect exact from={`/`} to={`/friend`} />
-    </Switch>
+    <SwitchRoutes />
   </Router>
 )
